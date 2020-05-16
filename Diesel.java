9f61134056b67dff58b3234bfeba793333977396
Czoso <64793134+Czoso@users.noreply.github.com>
2020-05-16 13:50:56 +0200

    Add files via upload

---
+++ Diesel.java
@@ -0,0 +1,7 @@
+public class Diesel extends Car
+{
+    public Diesel(String model, String manufacturer, int price, int year, double engineCapacity)
+    {
+        super(model, manufacturer, price, year, engineCapacity);
+    }
+}
---
+++ LPG.java
@@ -0,0 +1,7 @@
+public class LPG extends Car
+{
+    public LPG(String model, String manufacturer, int price, int year, double engineCapacity)
+    {
+        super(model, manufacturer, price, year, engineCapacity);
+    }
+}
